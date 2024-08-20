f05930a7d09b15895dd19fbaa26ede10e444287b
Helio Araujo e Castro <heliocastro@nuclep.gov.br>
2024-08-20 14:47:59 -0300

    Create atividade-3.py

---
+++ ILCA/lista-2/atividade-3.py
@@ -0,0 +1,16 @@
+# encoding: iso-8859-1
+
+num1 = int(input("Informe um número:"))
+num2 = int(input("Informe outro número:"))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print("Para os dois números: ", num1, " e ", num2)
+print("--------------------------------")
+print("Soma = ", soma)
+print("Subtraç?o = ", subtracao)
+print("Multiplicaç?o = ", multiplicacao)
+print("Divis?o = ", divisao)
