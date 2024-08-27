8ffd26f7c6d06825642aa048b132cd8e16820925
Helio Araujo e Castro <heliocastro@nuclep.gov.br>
2024-08-27 09:44:00 -0300

    Update atividade-17.py

--- ILCA/lista-3/atividade-17.py
+++ ILCA/lista-3/atividade-17.py
@@ -1,17 +1,36 @@
-# Lista com os 50 primeiros n√∫meros primos
-primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+# encoding: iso-8859-1
+
+# Lista com os 50 primeiros numeros primos
+# Desta forma o m·ximo n˙mero perfeito È o oitavo n˙mero: 2305843008139952128
+primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 ]
 resultado = []
 soma = 0
+
+def gera_divisores(num):
+    # Faz a chamada da funÁ?o recursiva divisores()
+    # ApÛs o processamento da vari·vel resultado,
+    # È realizado o pÛs-processamento dos divisores repetidos, multiplicando-os
+    # alÈm de adicionar o divisor 1 comum a todos os n˙meros
+    
+    divisores(num)
+    resultado.append(1)
+
+    anterior = resultado[0]
+    for i in range(1,len(resultado)):
+        if anterior == resultado[i]:
+            resultado[i] = resultado[i-1] * resultado[i]
+        else:
+            anterior = resultado[i]
 
 def divisores(num):
     if (num > 1):
 
         for primo in primos:
-            if (num % primo == 0):  #√â divis√≠vel pelo fator primo
+            if (num % primo == 0):  #â divis≠vel pelo fator primo
                 fator_primo = primo
 
-                if (num != fator_primo): #N√£o busca novos divisores quanto o n√∫mero √© um n√∫mero primo
-                    # Divide o n√∫mero para encontrar o divisor
+                if (num != fator_primo): #Noo busca novos divisores quanto o numero eh um numero primo
+                    # Divide o numero para encontrar o divisor
                     divisor = int(num/primo)
                     
                     # Adiciona o valor encontrado na lista
@@ -20,24 +39,23 @@ def divisores(num):
                     # Chamada recursiva para identificar outros divisores a partir do divisor encontrado
                     divisores(divisor)
 
-                # Adiciona o divisor primo se n√£o estiver na lista
-                if (fator_primo < resultado[-1]):
+                    # Adiciona o divisor primo se nao estiver na lista
                     resultado.append(fator_primo)
 
                 break  # Interrompe o loop quando localizar o menor divisor
-            
-    else:
-        resultado.append(1)
 
-numero = int(input("Informe um n√∫mero para verificar se √© perfeito "))
 
-divisores(numero)
+numero = int(input("Informe um numero para verificar se e perfeito "))
+
+gera_divisores(numero)
+resultado.sort(reverse=True)
+
 for n in resultado:
     soma = soma + n
 
 print("Divisores = " , resultado)
 print("Soma dos divisores = ", soma)
 if (numero == soma):
-    print("√â um n√∫mero perfeito")
+    print("E um numero perfeito")
 else:
-    print("N√£o √© um n√∫mero perfeito")
+    print("Nao e um numero perfeito")
